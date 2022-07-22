9e2b62a76608589d9f5371d68df3c54df484332e
Anthony Archie <aarchie1@hotmail.com>
2022-07-22 03:32:19 -0700

    7/22/22 RADS Work Log
    DungeonCharacter.java
    - Added fields myHealthPotions & myVisionPotions & myItems (arraylist)
    - Added static field: VISION_POTION_VALUE defaulted to 20 for the value of HP that a vision potion uses.
    	need to decide where this logic is going to live/ how we are going to handle this.
    - Added getter & setter methods for the added fields
    - Added checkAlive method that returns a boolean of true if the DungeonCharacter has HP > 0, and false if the HP has fallen
    	below 0.
    - Changed Most visibility to package level for the time being. Need to review encapsulation goals with team.
    
    CreatureEntityController.java
    - Added Default Constructor which requires an input String containing the type of Dungeon Character to be produced.
    	if theCharacter is a hero, this method defaults to spawning the warrior, if theCharacter is a monster this defaults to
    	a gremlin.
    - Added checkAlive method to return a boolean whether the player is alive or not (true if hp>0 false if hp<0)
    - Added giveItem method that takes a String theItem and addes it to the players arraylist of items and also returns the number
    	of items a player has on them as a int.
    - Added useHealthPotion method that calls for the character to use a health potion. (Need to discuss where the logic for this
    	needs to live, in CreatureEntityController or in DungeonCharacter.)
    - Added useVisionPotion method that calls for the character to use a vision potion (only reduces the amount in the players
    	inventory as the logic will be handled in mapModel).
    - Added checkAlive method that returns a boolean of true if the DungeonCharacter has HP > 0, and false if the HP has fallen
    	below 0.
    - Might be good to just add BattleLogic Functionality into CreatureEntityController rather than having a seperate class.
    	CreatureEntityController contains all the needed info to preform the battle. If anything BattleLogic should be a static
    	class that takes in two character (hero and a monster) and preforms the automated battle, reports the results, and
    	returns a string containing a winner of the battle. Then CreatureEntityController can decide what to do with this information.
    
    CreatureCrossover.java
    - Added a field CreatureEntityController.
    - Added Default Constructor which requires an input String containing the type of Dungeon Character to be produced.
    	this constructor initializes the CreatureEntityController to allow the RoomEntity package to interact with the CreatureEntity
    	package via the controller. This ultimately is the bridge between two packages so that the room can spawn the Hero In.
    - Added Comments into top of the file to better describe what the purpose of this class is.
    
    EntityController.java
    - Changed the addMonster method to reflect the changes in the CreatureCrossover constructor. The addMonster method now calls the
    	CreatureCrossover constructor with the String parameter "monster" which will creature a monster.
    
    RoomController.java
    
    EntityController.java
    - Uncommented line in genericRoom method to add functionality of adding a monster now that the addMonster method is compeleted.
    - Add Comments to existing methods and top method of file.

--- GameFiles/src/CreatureEntityModel/CreatureEntity.java
+++ GameFiles/src/CreatureEntityModel/CreatureEntity.java
@@ -12,4 +12,5 @@ public interface CreatureEntity {
     public String toString();
 
     String getMyName();
+
 }
--- GameFiles/src/RoomEntity/CreatureCrossover.java
+++ GameFiles/src/RoomEntity/CreatureCrossover.java
@@ -1,18 +1,26 @@
 package RoomEntity;
 
-
+/**
+ * This Class serves as the bridge between the CreatureEntityModel and the RoomEntityModel.
+ * This Class should instantiate a CreatureEntityController in order to make use of the controller
+ * to place monster and heroes in rooms as necessary.
+ * @author Anthony Archie
+ * @author Rowan W Osmon
+ * @version .01
+ */
 import CreatureEntityModel.CreatureEntityController;
 
 public class CreatureCrossover implements RoomEntity {
+    private final CreatureEntityController myCreature;
 
-    CreatureEntityController myCreature = new CreatureEntityController();
 
-    CreatureCrossover(){
-
+    CreatureCrossover(final String theCharacter){
+        myCreature = new CreatureEntityController(theCharacter);
     }
 
     @Override
     public String toString() {
-        return myCreature.getCreatureName();
+        return myCreature.getCharacterName();
     }
+
 }
