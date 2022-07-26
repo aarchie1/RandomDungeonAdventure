95bf7c25254cd1532af45afa0befb7dec3b6e594
Anthony Archie <aarchie1@hotmail.com>
2022-07-26 01:39:47 -0700

    7/26/22 RADS Work Log
    CreatureEntityControllerTest.java
    - Fixed setUp() @BeforeEach for tests
    - Updated getCharacterNames() test to make use of the new functionality of creatureEntityController.
    - Updated checkAlive() test to make use of the new functionality of creatureEntityController. Also checked to see if
    	a hero could take enough damage and the checkAlive would report back the correct state of a dead player.
    - Added testing for giving a character an item. TODO: FIGURE OUT IF WE ARE GOING TO CREATE A METHOD IN creatureEntityController.java
    	to allow outside reference to the list of items a character has. I believe we should have this method as the controller will need
    	to pass along the heros items at the end of the game to check if the win condition is satisfied.
    - Implemented testing for giving a character 4 items and checking to see how many items the character has.
    - Added a helperGiveHeroPillars method to give a hero 4 items.
    - Added a test called giveItem2() which gives the hero 4 pillars and checks to see if the player contains the four pillars in order
    	to set a win condition. The test checks the win condition.
    - Added a test called useHealthPotion() that gives a hero a health potion, checks to see whether that potion is added to the players inventory.
    	This test also uses a health potion of 20 points to check to see if the potion method is working correctly.
    - Added a test called useVisionPotion() that gives a hero a vision potion, checks to see whether that potion is added to the players inventory.
    	This test also removes check to see if the vision potion was remove from the players inventory.
    
    CreatureEntityController.java
    - Added method for creating an instance of a hero. This defaults to warrior right now but once the game is running we can refactor
    	so that all the player models can be choosen.
    - Added method for creating an instance of a monster. This defaults to gremlin right now but once the game is running we can refactor
    	so that all the enemy models can be choosen.
    - Added a getMyHero method to allow the controller to return a copy of the current Hero's name as a string
    - Added a getMyMonster method to allow the controller to return a copy of the current monsters name as a string
    - Added a getMyHeroItems method that returns the heros items as a long string with spaces inbetween item names. Can use the code from giveITem
    	test to turn this long string back into a list.
    
    CreatureCrossover.java
    - Updated the toString method as the controller contains a copy of the hero and monster. This means the to string should return the
    	current heros name as well as the current monsters name.
    - Updated the constructor as the constructure call no longer requires a parameter when creating a instance of a creatureEntityController.
    
    Hero.java
    - Added a method to remove items from a players inventory. This will be used by CreatureEntityController.java to remove health
    	and vision potions if a player uses them.

--- GameFiles/src/CreatureEntityModel/CreatureEntityControllerTest.java
+++ GameFiles/src/CreatureEntityModel/CreatureEntityControllerTest.java
@@ -5,41 +5,104 @@ import org.junit.jupiter.api.Test;
 import CreatureEntityModel.DungeonCharacter;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreatureEntityControllerTest {
-    CreatureEntityController myHero  = new CreatureEntityController("hero");
-    CreatureEntityController myMonster = new CreatureEntityController("Monster");
+    CreatureEntityController myController  = new CreatureEntityController();
+
 
 
     @BeforeEach
     void setUp(){
-        CreatureEntityController myHero  = new CreatureEntityController("hero");
-        CreatureEntityController myMonster = new CreatureEntityController("Monster");
+        myController.createHero();
+        myController.createMonster();
+
     }
 
     @Test
     void getCharacterNames() {
-        //System.out.println(myHero.getCharacterName());
-        assertEquals("Wyze the Warrior", myHero.getCharacterName());
-        assertEquals("Gizmo The Gremlin", myMonster.getCharacterName());
+        assertEquals("Wyze the Warrior", myController.getMyHero());
+        assertEquals("Gizmo The Gremlin", myController.getMyMonster());
     }
 
     @Test
     void checkAlive() {
-        System.out.println(myHero.checkAlive());
-
+        assertEquals(true, myController.checkHeroAlive());
+        myController.setHeroDamage(125);
+        assertEquals(false, myController.checkHeroAlive());
     }
 
     @Test
     void giveItem() {
+        helperGiveHeroPillars();
+        String str =  myController.getMyHeroItems();
+        String[] strSplit = str.split(" ");
+        ArrayList<String> strList = new ArrayList<String>(
+                Arrays.asList(strSplit));
+        int heroesItems = strList.size();
+        assertEquals(4, heroesItems);
+    }
+    @Test
+    void giveItem2() {
+        boolean winCondition = false;
+        helperGiveHeroPillars();
+        String str =  myController.getMyHeroItems();
+        String[] strSplit = str.split(" ");
+        ArrayList<String> strList = new ArrayList<String>(
+                Arrays.asList(strSplit));
+        int heroesItems = strList.size();
+        if (strList.contains("Pillar1") && strList.contains("Pillar2") &&
+                strList.contains("Pillar3") && strList.contains("Pillar4")) {
+            winCondition = true;
+        }
+        assertEquals(true, winCondition);
     }
 
     @Test
     void useHealthPotion() {
+        String str = "HealthPotion";
+        boolean condition = false;
+        myController.giveItem(str);
+        String heroItems = myController.getMyHeroItems();
+        if(str.equals(heroItems.substring(0,heroItems.length()-1))){
+            condition = true;
+        }
+        assertEquals(true , condition);
+        myController.setHeroDamage(125);
+        assertEquals(false, myController.checkHeroAlive());
+        myController.useHealthPotion(String.valueOf(20));
+        assertEquals(true, myController.checkHeroAlive());
+        System.out.println(myController.checkHeroAlive());
     }
 
     @Test
     void useVisionPotion() {
+        ArrayList<String> strList = new ArrayList<String>();
+        String item1 = "VisionPotion";
+        boolean condition = false;
+        myController.giveItem(item1);
+        String heroItems = myController.getMyHeroItems();
+        if(item1.equals(heroItems.substring(0,heroItems.length()-1))){
+            condition = true;
+        }
+        assertEquals(true , condition);
+        myController.useVisionPotion();
+        String str =  myController.getMyHeroItems();
+        String[] strSplit = str.split(" ");
+        if( strSplit == null){
+            strList = new ArrayList<String>(Arrays.asList(strSplit));
+        }
+        int heroesItems = strList.size();
+        assertEquals(0, heroesItems);
+    }
+
+    private void helperGiveHeroPillars() {
+        myController.giveItem("Pillar1");
+        myController.giveItem("Pillar2");
+        myController.giveItem("Pillar3");
+        myController.giveItem("Pillar4");
     }
 }
